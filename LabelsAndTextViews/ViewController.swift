3a43d37a7fcba6eda46ad6afc54b9cbd313297a7
Michael Kozub <michaelkozub@gmail.com>
2019-05-02 12:51:11 -0600

    empty project

---
+++ LabelsAndTextViews/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LabelsAndTextViews
+//
+//  Created by Michael Kozub on 5/2/19.
+//  Copyright © 2019 Michael Kozub. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+}
+
