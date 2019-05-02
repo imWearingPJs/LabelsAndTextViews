852600f2140cab048cdaa8f0e28ed3eebe8c20d1
Michael Kozub <michaelkozub@gmail.com>
2019-05-02 15:18:29 -0600

    adding label and text view, constraints, etc

--- LabelsAndTextViews/ViewController.swift
+++ LabelsAndTextViews/ViewController.swift
@@ -7,12 +7,46 @@
 //
 
 import UIKit
+import Anchorage
 
 class ViewController: UIViewController {
+    
+    let testLabel = UILabel()
+    let testTextView = UITextView()
+    let testText = "Lorem ipsum dolor sit amet, vel at idque vulputate concludaturque, ne eum erat bonorum officiis. Et pri dicam quaerendum, pro simul consul sadipscing id. Et mediocrem efficiantur liberavisse usu, per no prompta virtute adversarium. Sonet philosophia cum no."
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        addViews()
+        addText()
+    }
+    
+    func addViews() {
+        self.view.addSubview(testLabel)
+        testLabel.backgroundColor = #colorLiteral(red: 0.8823529412, green: 0.3450980392, blue: 0.1607843137, alpha: 1)
+        testLabel.widthAnchor == self.view.safeAreaLayoutGuide.widthAnchor / 2
+        testLabel.heightAnchor == self.view.safeAreaLayoutGuide.heightAnchor / 10
+        testLabel.centerXAnchor == self.view.safeAreaLayoutGuide.centerXAnchor
+        testLabel.topAnchor == self.view.safeAreaLayoutGuide.topAnchor + 25
+        testLabel.numberOfLines = 0
+//        testLabel.attributedText = NSAttributedString(string: "www.google.com")
+        
+        self.view.addSubview(testTextView)
+        testTextView.backgroundColor = #colorLiteral(red: 0.8823529412, green: 0.3450980392, blue: 0.1607843137, alpha: 1)
+        testTextView.widthAnchor == self.view.safeAreaLayoutGuide.widthAnchor / 2
+        testTextView.heightAnchor == self.view.safeAreaLayoutGuide.heightAnchor / 10
+        testTextView.centerXAnchor == self.view.safeAreaLayoutGuide.centerXAnchor
+        testTextView.topAnchor == testLabel.safeAreaLayoutGuide.bottomAnchor + 25
+        testTextView.isEditable = false
+
+    }
+    
+    func addText() {
+        testLabel.text = testText
+        testLabel.font = UIFont(name: "Arial", size: 18)
+        
+        testTextView.text = testText
+        testTextView.font = UIFont(name: "Arial", size: 18)
     }
 
 }
